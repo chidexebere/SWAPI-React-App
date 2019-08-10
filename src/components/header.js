0e0d3bf421c6012a9ee794b37dffecda256e4c61
Chidiebere <chidex5@gmail.com>
2019-08-10 10:45:30 +0100

    feat: Create Layout

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <section className="header">
+      <h1> header </h1>
+    </section>
+  );
+}
+
+export default Header;
---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main(props) {
+  return (
+    <section className="main">
+      <h1> main </h1>
+    </section>
+  );
+}
+
+export default Main;
