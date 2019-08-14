f5a0415431192ff93a838be36d06a3cecad342b9
Chidiebere <chidex5@gmail.com>
2019-08-14 23:02:43 +0100

    chore: Run Test Build

---
+++ build/precache-manifest.c94d261a41705c9c4e72bf198113d479.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ef8cbe96e86cd3f50d3d4378c6ba98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30343a1a56c4c1fda2d7",
+    "url": "/static/css/main.f09a40e5.chunk.css"
+  },
+  {
+    "revision": "72d781971b253400c1b3",
+    "url": "/static/js/2.aca57129.chunk.js"
+  },
+  {
+    "revision": "30343a1a56c4c1fda2d7",
+    "url": "/static/js/main.1636c056.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd4c8bebf90b82f0fb4d072e97d9c9d8",
+    "url": "/static/media/hero-banner.dd4c8beb.jpg"
+  },
+  {
+    "revision": "4df52cf60042640a0ea102e65fda59e7",
+    "url": "/static/media/starship-2.4df52cf6.jpg"
+  }
+]);
