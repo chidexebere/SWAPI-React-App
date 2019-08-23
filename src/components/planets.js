6076db8043f4080cf758eca5a963ae6543ac5e3c
Chidiebere <chidex5@gmail.com>
2019-08-23 05:37:52 +0100

    feat: Create planets components

---
+++ src/components/planets.js
@@ -0,0 +1,304 @@
+import React from "react";
+
+function Planets(props) {
+  return (
+    <section className="planets">
+      <div id="demo" class="carousel slide" data-ride="carousel">
+        <ul class="carousel-indicators">
+          <li data-target="#demo" data-slide-to="0" class="active" />
+          <li data-target="#demo" data-slide-to="1" />
+          <li data-target="#demo" data-slide-to="2" />
+        </ul>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container">
+              <div class="card-deck mb-3 text-center">
+                <div class="card mb-4 shadow-sm">
+                  <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Free</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">
+                      $0 <small class="text-muted">/ mo</small>
+                    </h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>10 users included</li>
+                      <li>2 GB of storage</li>
+                      <li>Email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type="button"
+                      class="btn btn-lg btn-block btn-outline-primary"
+                    >
+                      Sign up for free
+                    </button>
+                  </div>
+                </div>
+                <div class="card mb-4 shadow-sm">
+                  <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Pro</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">
+                      $15 <small class="text-muted">/ mo</small>
+                    </h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type="button"
+                      class="btn btn-lg btn-block btn-primary"
+                    >
+                      Get started
+                    </button>
+                  </div>
+                </div>
+                <div class="card mb-4 shadow-sm">
+                  <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Enterprise</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">
+                      $29 <small class="text-muted">/ mo</small>
+                    </h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>30 users included</li>
+                      <li>15 GB of storage</li>
+                      <li>Phone and email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type="button"
+                      class="btn btn-lg btn-block btn-primary"
+                    >
+                      Contact us
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container">
+              <div class="card-deck mb-3 text-center">
+                <div class="card mb-4 shadow-sm">
+                  <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Free</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">
+                      $0 <small class="text-muted">/ mo</small>
+                    </h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>10 users included</li>
+                      <li>2 GB of storage</li>
+                      <li>Email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type="button"
+                      class="btn btn-lg btn-block btn-outline-primary"
+                    >
+                      Sign up for free
+                    </button>
+                  </div>
+                </div>
+                <div class="card mb-4 shadow-sm">
+                  <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Pro</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">
+                      $15 <small class="text-muted">/ mo</small>
+                    </h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type="button"
+                      class="btn btn-lg btn-block btn-primary"
+                    >
+                      Get started
+                    </button>
+                  </div>
+                </div>
+                <div class="card mb-4 shadow-sm">
+                  <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Enterprise</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">
+                      $29 <small class="text-muted">/ mo</small>
+                    </h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>30 users included</li>
+                      <li>15 GB of storage</li>
+                      <li>Phone and email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type="button"
+                      class="btn btn-lg btn-block btn-primary"
+                    >
+                      Contact us
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container">
+              <div class="card-deck mb-3 text-center">
+                <div class="card mb-4 shadow-sm">
+                  <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Free</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">
+                      $0 <small class="text-muted">/ mo</small>
+                    </h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>10 users included</li>
+                      <li>2 GB of storage</li>
+                      <li>Email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type="button"
+                      class="btn btn-lg btn-block btn-outline-primary"
+                    >
+                      Sign up for free
+                    </button>
+                  </div>
+                </div>
+                <div class="card mb-4 shadow-sm">
+                  <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Pro</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">
+                      $15 <small class="text-muted">/ mo</small>
+                    </h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type="button"
+                      class="btn btn-lg btn-block btn-primary"
+                    >
+                      Get started
+                    </button>
+                  </div>
+                </div>
+                <div class="card mb-4 shadow-sm">
+                  <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Enterprise</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">
+                      $29 <small class="text-muted">/ mo</small>
+                    </h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>30 users included</li>
+                      <li>15 GB of storage</li>
+                      <li>Phone and email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type="button"
+                      class="btn btn-lg btn-block btn-primary"
+                    >
+                      Contact us
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <span class="carousel-control-prev-icon" />
+          </a>
+          <a class="carousel-control-next" href="#demo" data-slide="next">
+            <span class="carousel-control-next-icon" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Planets;
+
+// class Planets extends Component {
+
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       properties: data.properties,
+//       property: data.properties[0]
+//     }
+//   }
+
+//   nextProperty = () => {
+//     const newIndex = this.state.property.index+1;
+//     this.setState({
+//       property: data.properties[newIndex]
+//     })
+//   }
+
+//   prevProperty = () => {
+//     const newIndex = this.state.property.index-1;
+//     this.setState({
+//       property: data.properties[newIndex]
+//     })
+//   }
+
+//   render() {
+//     const {properties, property} = this.state;
+//     return (
+//       <div className="App">
+
+//         <button
+//           onClick={() => this.nextProperty()}
+//           disabled={property.index === data.properties.length-1}
+//         >Next</button>
+//         <button
+//           onClick={() => this.prevProperty()}
+//           disabled={property.index === 0}
+//         >Prev</button>
+
+//         <div className="page">
+//             <section>
+//                 <img src={logo} className="App-logo" alt="logo" />
+//                 <h1>Image slideshow React tutorial.</h1>
+//             </section>
+
+//             <div className="col">
+//               <div className={`cards-slider active-slide-${property.index}`}>
+//                 <div className="cards-slider-wrapper" style={{
+//                   'transform': `translateX(-${property.index*(100/properties.length)}%)`
+//                 }}>
+//                   {
+//                     properties.map(property => <Card key={property._id} property={property} />)
+//                   }
+//                 </div>
+//               </div>
+//             </div>
+
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default Planets;
