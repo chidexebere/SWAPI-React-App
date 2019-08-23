2ae25124b2fa971d2be7f03b462fc3b0af3af8f0
Chidiebere <chidex5@gmail.com>
2019-08-23 06:54:47 +0100

    Chore: Add dummy data

---
+++ src/data/planetData.js
@@ -0,0 +1,18 @@
+const planets = [
+  {
+    id: 1,
+    name: "Corellia",
+    imgSrc: require("../assets/planet-1.jpg")
+  },
+  {
+    id: 2,
+    name: "Ord Mantell",
+    imgSrc: require("../assets/planet-2.jpg")
+  },
+  {
+    id: 3,
+    name: "Endor",
+    imgSrc: require("../assets/planet-3.jpg")
+  }
+];
+export default planets;
---
+++ src/data/shipData.js
@@ -0,0 +1,46 @@
+const starShips = [
+  {
+    id: 1,
+    name: "Ghost",
+    des:
+      "The Ghost is a modified VCX-100 light freighter, manufactured by the Corellian Engineering Cooperation.",
+    imgSrc: require("../assets/starship-2.jpg")
+  },
+  {
+    id: 2,
+    name: "Correllian Scout",
+    des:
+      "Loronar E-9 Explorer was a transport used by the Rebel Alliance during tht fight against the Galactic Empire during the Galactic Civil War.",
+    imgSrc: require("../assets/starship-5.jpg")
+  },
+  {
+    id: 3,
+    name: "Raven's Claw",
+    des:
+      "The Raven's Claw was a fast light courier that replaced Kyle Katarn's old ship, the Moldy Crow.",
+    imgSrc: require("../assets/starship-3.jpg")
+  },
+  {
+    id: 4,
+    name: "Loronar E-9 Explorer",
+    des:
+      "Loronar E-9 Explorer was a transport used by the Rebel Alliance during tht fight against the Galactic Empire during the Galactic Civil War.",
+    imgSrc: require("../assets/starship-4.jpg")
+  },
+  {
+    id: 5,
+    name: "Millennium Falcon",
+    des:
+      "Loronar E-9 Explorer was a transport used by the Rebel Alliance during tht fight against the Galactic Empire during the Galactic Civil War.",
+    imgSrc: require("../assets/starship-1.jpg")
+  },
+  {
+    id: 6,
+    name: "Outrider",
+    des:
+      "The Outrider was a heavily modified YT-2400 light frieghter used by Dash Rendar in Shadows of the Empire.",
+    imgSrc: require("../assets/starship-6.jpg")
+  }
+];
+
+export default starShips;
